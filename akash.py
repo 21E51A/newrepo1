29bbbcd6f16fb6d4802bfac868ea7a98b2078151
21E51A <akashpaiavula2003@gmail.com>
2025-10-23 20:10:23 +0530

    akash.py

--- akash.py
+++ akash.py
@@ -4,13 +4,13 @@ def greet(name):
     print(f"Hello, {name}! Welcome to Python programming.")
 
 def add_numbers(a, b):
-    return a + b - v
+    return a + b 
 
 # Main program
 if __name__ == "__main__":
     greet("Akash")
 
-    num1 = 10
+    num1 = 100
     num2 = 200
     result = add_numbers(num1, num2)
     print(f"The sum of {num1} and {num2} is {result}.")
